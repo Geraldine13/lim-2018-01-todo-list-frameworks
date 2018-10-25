4f2002010be56dd346373a27bf5d55dc0bce2f8f
Geraldine13 <geral1312@hotmail.com>
2018-10-25 16:07:13 -0500

    Agregando firebase a environment

--- proyecto-todo-list/src/environments/environment.prod.ts
+++ proyecto-todo-list/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyDLrGhRdJ_V8iNUAfqyh4LqilGgEIL_9kQ",
+    authDomain: "to-do-list-952dd.firebaseapp.com",
+    databaseURL: "https://to-do-list-952dd.firebaseio.com",
+    projectId: "to-do-list-952dd",
+    storageBucket: "to-do-list-952dd.appspot.com",
+    messagingSenderId: "681108844523"
+  }
+
 };
